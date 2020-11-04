6695cb06e421867a23fd5c6d0e9565df1ab47f8b
Gabriel Toschi <gabriel.toschi@hotmail.com>
2020-11-03 22:38:41 -0300

    Create basic game loop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumberIndicatorComponent } from './components/number-indicator/number-indicator.component';
+import { TriviaCardComponent } from './components/trivia-card/trivia-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberIndicatorComponent,
+    TriviaCardComponent
   ],
   imports: [
     BrowserModule,
